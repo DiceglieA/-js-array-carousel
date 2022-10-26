17b90e48bfacdf26fd2269b4b69b76debba2f933
DiceglieA <angelodiceglie10@gmail.com>
2022-10-26 18:12:10 +0200

    n tentativo di loop

---
+++ bonus 1/script/main.js
@@ -0,0 +1,57 @@
+const arrImages = [	
+    'img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg',
+];
+
+const eleSlider = document.querySelector('.slider');
+const eleBtnTop = document.querySelector('.btn-top');
+const eleBtnBottom = document.querySelector('.btn-bottom');
+
+for (let i = 0; i < arrImages.length; i++) {
+	const eleImg = document.createElement('img');
+	eleImg.src = arrImages[i];
+	eleImg.classList.add('slider-img');
+
+	if (i === 0) {
+		eleImg.classList.add('active');
+	}
+
+	eleSlider.append(eleImg);
+}
+
+const listEleImg = document.querySelectorAll('.slider-img');
+
+let activeCounter = 0
+
+eleBtnBottom.addEventListener('click', function () {
+
+    listEleImg[activeCounter].classList.remove('active');
+
+	if (activeCounter === 4) {
+		activeCounter = -1;
+	}
+	
+    activeCounter++;
+
+    listEleImg[activeCounter].classList.add('active');
+
+	
+	
+	
+
+});
+
+eleBtnTop.addEventListener('click', function () {
+	listEleImg[activeCounter].classList.remove('active');
+	
+	
+	if (activeCounter === 0) {
+		activeCounter = 5;
+	}
+
+	activeCounter--;
+
+	listEleImg[activeCounter].classList.add('active');
+
+});
+
+
